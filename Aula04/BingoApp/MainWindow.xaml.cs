d2048296ec41b8257d378094e1f1a1f5fb90aa65
Gilbert Azevedo da Silva <1164676@ifrn.local>
2022-11-11 15:04:28 -0300

    Bingo

--- Aula04/BingoApp/MainWindow.xaml.cs
+++ Aula04/BingoApp/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace BingoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Bingo b;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +30,35 @@ namespace BingoApp
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             txtNumBolas.Text = slider.Value.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b = new Bingo();
+            b.Iniciar(int.Parse(txtNumBolas.Text));
+            btnIniciar.IsEnabled = false;
+            btnSortear.IsEnabled = true;
+            txtNum.Clear();
+            txtSorteados.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int x = b.Proximo();
+            if (x == -1) // Game Over
+            {
+                txtNum.Text = "Fim";
+                btnIniciar.IsEnabled = true;
+                btnSortear.IsEnabled = false;
+            }
+            else
+            {
+                txtNum.Text = x.ToString();
+                string s = "";
+                foreach (int i in b.Sorteados())
+                    s = s + i + " - ";
+                txtSorteados.Text = s;
+            }
         }
     }
 }
