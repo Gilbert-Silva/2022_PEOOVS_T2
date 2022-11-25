1f074f6eeb578636633963692d23a81ba213aad6
Gilbert Azevedo da Silva <1164676@ifrn.local>
2022-11-25 16:20:21 -0300

    Empresa

---
+++ Aula05/EmpresaApp/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaApp
+{
+    class Cliente
+    {
+        private string nome;
+        private double limite = 100;
+        public Cliente(string nome, double limite)
+        {
+            this.nome = nome;
+            if (limite > 0) this.limite = limite;
+        }
+        public override string ToString()
+        {
+            return $"Cliente = {nome} - Limite = {limite}";
+        }
+    }
+}
---
+++ Aula05/EmpresaApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmpresaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Empresa emp;
+
+        private void btnEmpresa_Click(object sender, RoutedEventArgs e)
+        {
+            emp = new Empresa(txtEmpresa.Text);
+            MessageBox.Show(emp.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente x = new Cliente(txtCliente.Text,
+                double.Parse(txtLimite.Text));
+            emp.Inserir(x);
+            // Insere um a um os objetos no ListBox
+            // listCliente.Items.Add(x);
+            // Insere um vetor num ListBox
+            listCliente.ItemsSource = null;
+            listCliente.ItemsSource = emp.Listar2();
+        }
+    }
+}
